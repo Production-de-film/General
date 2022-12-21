c490871fcc0d6f801a6fe9dcee8a2cf03b0814b2
Antony013 <83763776+Antony013@users.noreply.github.com>
2022-12-21 09:31:17 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -8,14 +8,3 @@ from selenium.common.exceptions import TimeoutException
 from selenium.common.exceptions import ElementNotInteractableException
 from selenium.common.exceptions import NoSuchWindowException
 from PIL import Image
-
-# on config la page de façon à ce que le bandeau "chrome is being controlled by automated test software" ne soit pas affiché
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_experimental_option("excludeSwitches", ['enable-automation'])
-# on lance le driver avec la config
-driver = webdriver.Chrome(options=chrome_options)
-# mettre la page en grand écran
-driver.maximize_window()
-
-# delay va servir si au bout de 10sec aucune réponse de la page alors on coupe 
-delay = 10
